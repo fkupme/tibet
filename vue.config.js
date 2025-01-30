2eb8467677b43f1261f270f77a976a72c8713241
V. Kirichenko <fkupme@yandex.ru>
2025-01-31 01:00:28 +0300

    vercel config upd

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,16 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-	lintOnSave: true,
-	transpileDependencies: true,
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'@': path.resolve(__dirname, 'src')
-			}
-		}
-	}
+  lintOnSave: true,
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    }
+  }
 });
